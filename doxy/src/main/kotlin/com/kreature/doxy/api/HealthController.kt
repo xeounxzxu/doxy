691b8e1b3670188682e3a77a27e7c6355ec47428
xeounxzxu <pnw123456@icloud.com>
2025-12-24 14:09:49 +0900

    feat: log add

--- doxy/src/main/kotlin/com/kreature/doxy/api/HealthController.kt
+++ doxy/src/main/kotlin/com/kreature/doxy/api/HealthController.kt
@@ -1,8 +1,11 @@
 package com.kreature.doxy.api
 
+import mu.KotlinLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+
+private val log = KotlinLogging.logger {}
 
 @RestController
 @RequestMapping("/api")
@@ -10,6 +13,7 @@ class HealthController {
 
     @GetMapping("/health")
     fun health(): String {
-        return "Deliverus service is up"
+        log.info { "health() called" }
+        return "Doxy Service up and running!"
     }
 }
