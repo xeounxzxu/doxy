9cf91079b6569aa0c67100dabbe9df64cbd05b11
xeounxzxu <pnw123456@icloud.com>
2025-12-24 13:22:34 +0900

    first commit

---
+++ src/main/kotlin/com/deliverus/poc/DeliverusPocApplication.kt
@@ -0,0 +1,11 @@
+package com.deliverus.poc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeliverusPocApplication
+
+fun main(args: Array<String>) {
+    runApplication<DeliverusPocApplication>(*args)
+}
---
+++ src/test/kotlin/com/deliverus/poc/DeliverusPocApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.deliverus.poc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeliverusPocApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
